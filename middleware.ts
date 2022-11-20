6a8c6efe3c5b9f46ca11e8f79d34b88e34b4a812
Agu Chux <agu.chux@yahoo.com>
2022-11-20 20:04:11 +0100

    domain

--- middleware.ts
+++ middleware.ts
@@ -9,9 +9,9 @@ export default async function middleware(req: NextRequest, res: any) {
   const { hostname } = nextUrl;
   let newUrl = url;
 
-  // if (hostname == "metrics.ng") {
-  //   return NextResponse.rewrite(`${newUrl}`);
-  // }
+  if (hostname == "metrics.ng") {
+    return NextResponse.rewrite(`${newUrl}`);
+  }
 
   // const response = await fetch(process.env.DOMAIN + `/api/domains/${hostname}`);
   // const result = await response.json();
