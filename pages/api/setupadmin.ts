d409072612ad158ec13ac250d741198f5a99bce9
Agu Chux <agu.chux@yahoo.com>
2022-12-02 23:03:59 +0100

    ESUT

--- pages/api/setupadmin.ts
+++ pages/api/setupadmin.ts
@@ -19,7 +19,7 @@ export default async function handler(
       // Encrypt Password//
       const { Accounts } = await dbCon();
       const created = await Accounts.create({
-        schoolid: "63877dbe1332f0c4be5a0395",
+        schoolid: "638a7617dfa1be141ca74281",
         email: "agu.chux@yahoo.com",
         firstname: "Agu",
         lastname: "Chux",
