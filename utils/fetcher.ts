fa3ffea1392ac9a82f462856d3b201ee99a7f8d1
Agu Chux <agu.chux@yahoo.com>
2022-12-16 11:47:53 +0100

    Added ESUT Admin

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -4,17 +4,3 @@ export const fetcher = async (url: string) =>
     .then((json) => {
       return json;
     });
-
-
-export const postFetcher = async ({url: string,body:object}:any) =>
-  await fetch(url,{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      return json;
-    });
